74b5c6dd3ae6bebe081757a586e7005b5fa2a013
kennWNK97 <castro21.kc@gmail.com>
2020-06-22 15:49:10 -0600

    modificacion

--- src/java/pdf_r.java
+++ src/java/pdf_r.java
@@ -1,27 +1,41 @@
 
-import com.itextpdf.text.*;
+
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.sql.*;
-
-@WebServlet(urlPatterns = {"/pdf"})
-public class pdf extends HttpServlet {
 
 
-protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-throws ServletException, IOException {
+@WebServlet(urlPatterns = {"/pdf_r"})
+public class pdf_r extends HttpServlet {
+
+  
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+ throws ServletException, IOException {
 response.setContentType("application/pdf");
 
 
 OutputStream out = response.getOutputStream();
+String n = request.getParameter("txtn");
 
 try {
 try{
@@ -33,7 +47,7 @@ ResultSet rs = null;
 Class.forName("com.mysql.jdbc.Driver");
 con = (Connection)DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_inventario", "root",  "");
 st = con.createStatement();
-rs= st.executeQuery("select * from  tb_categoria");
+rs= st.executeQuery("select * from  tb_categoria where  nom_categoria = '"+n+" ' ");
 
 
 if(con!=null){
@@ -106,8 +120,7 @@ out.close();
 }
     
 }
-
-
+    
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -115,32 +128,16 @@ out.close();
         processRequest(request, response);
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
+
     @Override
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
 
 }
-
-
-
-
